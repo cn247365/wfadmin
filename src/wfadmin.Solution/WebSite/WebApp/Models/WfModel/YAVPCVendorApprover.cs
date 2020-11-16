af9fa082cd1c92ea3ca924b74911639430dc8168
Hualin Zhu <zzHualin.zzZhu@voith.com>
2020-11-16 14:19:50 +0800

    YAVPCVendorApprover

---
+++ src/wfadmin.Solution/WebSite/WebApp/Models/WfModel/YAVPCVendorApprover.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Web;
+using SqlSugar;
+using TrackableEntities;
+
+namespace WebApp.Models.WfModel
+{
+  [SugarTable("YAVPCVendorApprover")]
+  public class YAVPCVendorApprover
+  {
+    [SugarColumn(IsPrimaryKey = true, IsIdentity = true)]
+    public int ID { get; set; }
+    public string Role { get; set; }
+    public string GlobalID { get; set; }
+    public string ShortName { get; set; }
+    public string DisplayName { get; set; }
+
+    [SugarColumn(IsIgnore = true)]
+    public TrackingState TrackingState { get; set; }
+
+  }
+}
