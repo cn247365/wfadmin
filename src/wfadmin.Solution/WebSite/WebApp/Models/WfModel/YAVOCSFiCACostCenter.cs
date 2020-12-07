3afd7f24549de1e28e8381329bd0f3c04a930e37
Hualin Zhu <zzHualin.zzZhu@voith.com>
2020-12-07 10:21:13 +0800

    commit

---
+++ src/wfadmin.Solution/WebSite/WebApp/Models/WfModel/YAVOCSFiCACostCenter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web;
+using Repository.Pattern.Ef6;
+using SqlSugar;
+using TrackableEntities;
+
+namespace WebApp.Models.WfModel
+{
+  [SugarTable("YAVOCSFiCACostCenter")]
+  public partial class YAVOCSFiCACostCenter
+  {
+    [SugarColumn(IsPrimaryKey = true, IsIdentity = true)]
+    public int ID { get; set; }
+    public string CostCenter { get; set; }
+    public string CCMGlobalID { get; set; }
+    public string CCMShortName { get; set; }
+    public string CCMDisplayName { get; set; }
+  
+    [SugarColumn(IsIgnore = true)]
+    public   TrackingState TrackingState{get;set;}
+
+
+
+  }
+}
---
+++ src/wfadmin.Solution/WebSite/WebApp/Models/WfModel/YAVTCNFiCostCenter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web;
+using Repository.Pattern.Ef6;
+using SqlSugar;
+using TrackableEntities;
+
+namespace WebApp.Models.WfModel
+{
+  [SugarTable("YAVTCNFiCostCenter")]
+  public partial class YAVTCNFiCostCenter
+  {
+    [SugarColumn(IsPrimaryKey = true, IsIdentity = true)]
+    public int ID { get; set; }
+    public string SiteID { get; set; }
+    public string CostCenter { get; set; }
+    public string Description { get; set; }
+    public int DivisionID { get; set; }
+    public string PersonResp { get; set; }
+    public string PersonShortName { get; set; }
+    public string PersonGlobalID { get; set; }
+    public int CCMLevel { get;set;}
+    public string SCCM1 { get; set; }
+    public string SCCM1ShortName { get; set; }
+    public string SCCM1GlobalID { get; set; }
+    public string SCCM2 { get; set; }
+    public string SCCM2ShortName { get; set; }
+    public string SCCM2GlobalID { get; set; }
+    [SugarColumn(IsIgnore = true)]
+    public   TrackingState TrackingState{get;set;}
+
+
+
+  }
+}
