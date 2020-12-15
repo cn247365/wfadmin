58abdc0400dc55b092724fcb007664c38d62db93
Hualin Zhu <zzHualin.zzZhu@voith.com>
2020-12-15 10:37:22 +0800

    add network entity

---
+++ src/wfadmin.Solution/WebSite/WebApp/Models/WfModel/YAVPCFiTANetWork.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web;
+using Repository.Pattern.Ef6;
+using SqlSugar;
+using TrackableEntities;
+
+namespace WebApp.Models.WfModel
+{
+  [SugarTable("YAVPCFiTANetWork")]
+  public partial class YAVPCFiTANetWork
+  {
+    [SugarColumn(IsPrimaryKey = true, IsIdentity = true)]
+    public int ID { get; set; }
+    public string Network { get; set; }
+    public string Description { get; set; }
+    public string Activity { get; set; }
+    public string Description2 { get; set; }
+    public string WBS { get; set; }
+    public DateTime UpdateTime { get; set; }
+    
+    [SugarColumn(IsIgnore = true)]
+    public   TrackingState TrackingState{get;set;}
+
+
+
+  }
+}
